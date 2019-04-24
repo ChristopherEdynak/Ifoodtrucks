a9947b8f3ced014020ac955df5115fbfd212e8e0
Christopher Edynak <christopher.m.a.edynak@gmail.com>
2019-04-24 14:45:37 -0400

    fixed the console.console.log();log the api

--- src/index.js
+++ src/index.js
@@ -21,6 +21,6 @@ app.use(bodyParser.json({
 app.use('/v1', routes);
 
 app.server.listen(config.port);
-console.log('Started on port ${ app.server.address().port }');
+console.log(`Started on port ${app.server.address().port}`);
 
 export default app;
